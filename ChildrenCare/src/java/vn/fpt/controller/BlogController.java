d6863314ae231cfc0bcfc247c8370fd5702b999a
dangvmhe164006 <dangvmhe164006@fpt.edu.vn>
2024-05-26 19:27:59 +0700

    fix

---
+++ ChildrenCare/src/java/vn/fpt/controller/BlogController.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package vn.fpt.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import vn.fpt.dao.BlogDAO;
+import vn.fpt.model.Blog;
+
+/**
+ *
+ * @author dangv
+ */
+public class BlogController extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet BlogController</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet BlogController at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+      request.getRequestDispatcher("Views/User/CreateBlog.jsp").forward(request, response);
+
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+BlogDAO blogDAO = new BlogDAO();
+     LocalDateTime now = LocalDateTime.now();
+     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+    String title = request.getParameter("title");
+    String content = request.getParameter("content");
+    String image = request.getParameter("image");
+    
+    Blog blog = new Blog();
+    blog.setTitle(title);
+    blog.setContent(content);
+    blog.setImage_url(image); 
+    blog.setCreate_at(now.format(formatter));
+    blog.setCreate_by(2);
+
+    int result = blogDAO.createBlog(blog);
+
+    if (result > 0) {
+        response.getWriter().write("Blog created successfully!");
+    } else {
+        response.getWriter().write("Failed to create blog.");
+    }
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
