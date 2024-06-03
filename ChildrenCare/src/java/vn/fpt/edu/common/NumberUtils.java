c93d35d05f82bd75bdf6a59db657ef7770e5896a
Tran Huu Thinh <thinhthhe163873@fpt.edu.vn>
2024-06-04 00:04:34 +0700

    Update validate integer data type ver 1

---
+++ ChildrenCare/src/java/vn/fpt/edu/common/NumberUtils.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vn.fpt.edu.common;
+
+/**
+ *
+ * @author THINH
+ */
+public class NumberUtils {
+    // Checks if a string is a valid integer
+        public static boolean isValidInteger(String str) {
+            if (str == null) {
+                return false;
+            }
+            try {
+                Integer.parseInt(str);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+            return true;
+        }
+}
