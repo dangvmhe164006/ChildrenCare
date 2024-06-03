9c409d73ac820731f9cde832bbe2477b92d85eb9
Tran Huu Thinh <thinhthhe163873@fpt.edu.vn>
2024-06-04 00:03:06 +0700

    Update validate date data type ver 1

---
+++ ChildrenCare/src/java/vn/fpt/edu/common/DateUtils.java
@@ -0,0 +1,14 @@
+
+package vn.fpt.edu.common;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author THINH
+ */
+public class DateUtils {
+    
+}
