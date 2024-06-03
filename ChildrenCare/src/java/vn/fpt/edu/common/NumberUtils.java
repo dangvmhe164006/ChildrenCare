e345eaed7be233d61ceb2cbd1a4689a8b0b176fb
Tran Huu Thinh <thinhthhe163873@fpt.edu.vn>
2024-06-04 00:05:25 +0700

    Update validate number data type final

--- ChildrenCare/src/java/vn/fpt/edu/common/NumberUtils.java
+++ ChildrenCare/src/java/vn/fpt/edu/common/NumberUtils.java
@@ -1,36 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package vn.fpt.edu.common;
 
-/**
- *
- * @author THINH
- */
 public class NumberUtils {
+
     // Checks if a string is a valid integer
-        public static boolean isValidInteger(String str) {
-            if (str == null) {
-                return false;
-            }
-            try {
-                Integer.parseInt(str);
-            } catch (NumberFormatException e) {
-                return false;
-            }
-            return true;
+    public static boolean isValidInteger(String str) {
+        if (str == null) {
+            return false;
         }
-        
-        public static boolean isValidDouble(String str) {
-            if (str == null) {
-                return false;
-            }
-            try {
-                Double.parseDouble(str);
-            } catch (NumberFormatException e) {
-                return false;
-            }
-            return true;
+        try {
+            Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    // Checks if a string is a valid double
+    public static boolean isValidDouble(String str) {
+        if (str == null) {
+            return false;
+        }
+        try {
+            Double.parseDouble(str);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 }
