67855b5ed5f429300d0125260d7ca9069b5c9d49
Tran Huu Thinh <thinhthhe163873@fpt.edu.vn>
2024-06-04 00:03:27 +0700

    Update validate date data type ver 2

--- ChildrenCare/src/java/vn/fpt/edu/common/DateUtils.java
+++ ChildrenCare/src/java/vn/fpt/edu/common/DateUtils.java
@@ -10,5 +10,19 @@ import java.util.Date;
  * @author THINH
  */
 public class DateUtils {
+    public static boolean isValidDate(String dateStr, String dateFormat) {
+        if (dateStr == null || dateFormat == null) {
+            return false;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+        sdf.setLenient(false);
+        try {
+            sdf.parse(dateStr);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
