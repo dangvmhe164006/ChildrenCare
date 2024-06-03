f1db0be3ab62b5077d4992fdd04dca948547087e
Tran Huu Thinh <thinhthhe163873@fpt.edu.vn>
2024-06-04 00:06:28 +0700

    Update validate string data type and validate email

--- ChildrenCare/src/java/vn/fpt/edu/common/StringUtils.java
+++ ChildrenCare/src/java/vn/fpt/edu/common/StringUtils.java
@@ -1,15 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package vn.fpt.edu.common;
 
-/**
- *
- * @author THINH
- */
 public class StringUtils {
+    // Checks if a string is not null and not empty
     public static boolean isNotEmpty(String str) {
         return str != null && !str.trim().isEmpty();
+    }
+
+    // Checks if a string is a valid email
+    public static boolean isValidEmail(String email) {
+        if (email == null) {
+            return false;
+        }
+        String emailRegex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        return email.matches(emailRegex);
     }
 }
