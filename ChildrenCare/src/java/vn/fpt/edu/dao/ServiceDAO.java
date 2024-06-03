0551782731e1fcc47152a17127a5af0e9cbe9b44
Tran Huu Thinh <thinhthhe163873@fpt.edu.vn>
2024-06-03 23:41:13 +0700

    Update service list

--- ChildrenCare/src/java/vn/fpt/edu/dao/ServiceDAO.java
+++ ChildrenCare/src/java/vn/fpt/edu/dao/ServiceDAO.java
@@ -4,3 +4,27 @@ import vn.fpt.edu.model.Service;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
+public class ServiceDAO extends DBContext {
+
+    public List<Service> getAllServices() {
+        List<Service> services = new ArrayList<>();
+        String query = "SELECT * FROM Services";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Service service = new Service(
+                    rs.getInt("ServiceID"),
+                    rs.getString("Name"),
+                    rs.getString("Description"),
+                    rs.getDouble("Price"),
+                    rs.getDate("DateCreated")
+                );
+                services.add(service);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return services;
+    }
