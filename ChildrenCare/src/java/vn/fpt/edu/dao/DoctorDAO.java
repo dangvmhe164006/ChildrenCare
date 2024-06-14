a3b35868cb02c45f5f0ff759691b5e0b80b0d6f6
Tran Huu Thinh <thinhthhe163873@fpt.edu.vn>
2024-06-14 13:15:11 +0700

    Update doc DAO ver 3

--- ChildrenCare/src/java/vn/fpt/edu/dao/DoctorDAO.java
+++ ChildrenCare/src/java/vn/fpt/edu/dao/DoctorDAO.java
@@ -76,3 +76,33 @@ public List<Staff> getDoctorByServices(String id) {
         return doctorList;
     }
 
+public List<Staff> getStaffsByRole(String staffRole) {
+        List<Staff> staffList = new ArrayList<>();
+        xSql = "SELECT *  FROM [dbo].[Staff] where StaffRole = ?";
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setString(1, staffRole);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int staffID = rs.getInt("StaffID");
+                String staffName = rs.getString("StaffName");
+                String password = rs.getString("Password");
+                String email = rs.getString("Email");
+                String fullName = rs.getString("FullName");
+                String gender = rs.getString("Gender");
+                String phoneNumber = rs.getString("PhoneNumber");
+                String profileImage = rs.getString("ProfileImage");
+                String role = rs.getString("StaffRole");
+                String rank = rs.getString("Rank");
+                String specialty = rs.getString("Specialty");
+                String introduction = rs.getString("Introduction");
+                Staff staff = new Staff(staffID, staffName, password, email, fullName, gender, phoneNumber, profileImage, role, rank, specialty, introduction);
+                staffList.add(staff);
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return staffList;
+    }
